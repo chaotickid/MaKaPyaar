18fd17b9201d4a0440894b6bcb0824a82b35c1b6
Aditya Patil <aditya.patil@mavenir.com>
2023-10-17 22:40:06 +0530

    first commit

---
+++ src/main/java/com/maakapyaar/MaakapyaarApplication.java
@@ -0,0 +1,13 @@
+package com.maakapyaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaakapyaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaakapyaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maakapyaar/MaakapyaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maakapyaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaakapyaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
