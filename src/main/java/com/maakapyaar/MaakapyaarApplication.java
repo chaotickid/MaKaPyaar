0af688fe4b01f53b26169014547630b28c844db9
Samadhan Borase <samadhan.borase@mavenir.com>
2023-10-18 11:26:03 +0530

    initial set for couchbase db

--- src/main/java/com/maakapyaar/MaakapyaarApplication.java
+++ src/main/java/com/maakapyaar/MaakapyaarApplication.java
@@ -1,13 +1,20 @@
 package com.maakapyaar;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MaakapyaarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaakapyaarApplication.class, args);
+		log.debug("\"********************* MaKaPyar *********************\\n\";\n" +
+				"\"* Application running on port:             [ OK ]*\\n\";\n" +
+				"\"* Couchbase connectionString:              [ OK ]*\\n\";\n" +
+				"\"****************************************************\"");
 	}
 
 }
