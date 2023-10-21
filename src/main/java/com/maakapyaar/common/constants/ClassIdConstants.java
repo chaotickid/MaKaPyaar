5d8e6adf8671dfa8f313938330fbe9d42b756f79
Aditya Patil <aditya.patil@mavenir.com>
2023-10-21 14:27:28 +0530

    fixed

--- src/main/java/com/maakapyaar/common/constants/ClassIdConstants.java
+++
@@ -1,6 +0,0 @@
-package com.maakapyaar.common.constants;
-
-public class ClassIdConstants {
-
-    public static final String USER_CLASS_ID_CONSTANT = "1000";
-}
--- src/main/java/com/maakapyaar/common/constants/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.maakapyaar.common.constants;
-
-public class Constants {
-    public static final String DOUBLE_COLON = "::";
-}
